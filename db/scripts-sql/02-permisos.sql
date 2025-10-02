d45dc728ce9ef84c6f360f5e0e43ba1dd095ce46
desmoris <damianesmoris@gmail.com>
2025-10-01 21:39:06 -0300

    Usuario `api_wallet` añadido

--- db/scripts-sql/02-permisos.sql
+++ db/scripts-sql/02-permisos.sql
@@ -1,35 +1,54 @@
 USE keymasters;
 
-CREATE USER IF NOT EXISTS 'cliente'@'localhost' IDENTIFIED BY 'cliente_password';
-CREATE USER IF NOT EXISTS 'publicador'@'localhost' IDENTIFIED BY 'publicador_password';
+CREATE USER IF NOT EXISTS 'api_cliente'@'%' IDENTIFIED BY '.';
+CREATE USER IF NOT EXISTS 'api_publicador'@'%' IDENTIFIED BY '.';
+CREATE USER IF NOT EXISTS 'api_backoffice'@'%' IDENTIFIED BY '.';
+CREATE USER IF NOT EXISTS 'api_wallet'@'%' IDENTIFIED BY '.';
 
-GRANT SELECT ON * TO 'cliente'@'localhost';
-GRANT SELECT ON * TO 'publicador'@'localhost';
+GRANT SELECT ON * TO 'api_cliente'@'%';
+GRANT SELECT ON * TO 'api_publicador'@'%';
 
-GRANT INSERT, UPDATE ON Cliente TO 'cliente'@'localhost';
-GRANT INSERT, UPDATE ON Compra TO 'cliente'@'localhost';
-GRANT INSERT, UPDATE ON Reventa TO 'cliente'@'localhost';
-GRANT INSERT, UPDATE ON Wallet TO 'cliente'@'localhost';
-GRANT INSERT, UPDATE, DELETE ON Reporta_res TO 'cliente'@'localhost';
-GRANT INSERT, UPDATE, DELETE ON Reporta_com TO 'cliente'@'localhost';
-GRANT INSERT, UPDATE, DELETE ON Reporta_pub TO 'cliente'@'localhost';
-GRANT INSERT, UPDATE, DELETE ON Favorito TO 'cliente'@'localhost';
-GRANT INSERT, UPDATE, DELETE ON Upovote TO 'cliente'@'localhost';
-GRANT INSERT, UPDATE ON Transaccion TO 'cliente'@'localhost';
-GRANT INSERT, UPDATE ON Movimiento TO 'cliente'@'localhost';
+GRANT INSERT, UPDATE ON cliente TO 'api_cliente'@'%';
+GRANT INSERT, UPDATE ON compra TO 'api_cliente'@'%';
+GRANT INSERT, UPDATE ON reventa TO 'api_cliente'@'%';
+GRANT INSERT, UPDATE, DELETE ON reporta_res TO 'api_cliente'@'%';
+GRANT INSERT, UPDATE, DELETE ON reporta_com TO 'api_cliente'@'%';
+GRANT INSERT, UPDATE, DELETE ON reporta_pub TO 'api_cliente'@'%';
+GRANT INSERT, UPDATE, DELETE ON favorito TO 'api_cliente'@'%';
+GRANT INSERT, UPDATE, DELETE ON upovote TO 'api_cliente'@'%';
+GRANT INSERT, UPDATE ON transaccion TO 'api_cliente'@'%';
+GRANT INSERT, UPDATE ON movimiento TO 'api_cliente'@'%';
 
-GRANT INSERT, UPDATE ON Publicador TO 'publicador'@'localhost';
-GRANT INSERT, UPDATE ON Wallet TO 'cliente'@'localhost';
-GRANT INSERT, UPDATE, DELETE ON Juego TO 'publicador'@'localhost';
-GRANT INSERT, UPDATE, DELETE ON Publica TO 'publicador'@'localhost';
-GRANT INSERT, UPDATE, DELETE ON Clave TO 'publicador'@'localhost';
-GRANT INSERT, UPDATE, DELETE ON Transaccion TO 'publicador'@'localhost';
-GRANT INSERT, UPDATE, DELETE ON Movimiento TO 'publicador'@'localhost';
-GRANT INSERT, UPDATE, DELETE ON Oferta TO 'publicador'@'localhost';
-GRANT INSERT, UPDATE, DELETE ON Genera TO 'publicador'@'localhost';
-GRANT INSERT, UPDATE, DELETE ON Asociada TO 'publicador'@'localhost';
-GRANT INSERT, UPDATE, DELETE ON Referencia TO 'publicador'@'localhost';
-GRANT INSERT, UPDATE, DELETE ON Publicidad TO 'publicador'@'localhost';
-GRANT INSERT, UPDATE, DELETE ON Media TO 'publicador'@'localhost';
+GRANT INSERT, UPDATE ON publicador TO 'api_publicador'@'%';
+GRANT INSERT, UPDATE, DELETE ON juego TO 'api_publicador'@'%';
+GRANT INSERT, UPDATE, DELETE ON publica TO 'api_publicador'@'%';
+GRANT INSERT, UPDATE, DELETE ON clave TO 'api_publicador'@'%';
+GRANT INSERT, UPDATE, DELETE ON transaccion TO 'api_publicador'@'%';
+GRANT INSERT, UPDATE, DELETE ON movimiento TO 'api_publicador'@'%';
+GRANT INSERT, UPDATE, DELETE ON oferta TO 'api_publicador'@'%';
+GRANT INSERT, UPDATE, DELETE ON genera TO 'api_publicador'@'%';
+GRANT INSERT, UPDATE, DELETE ON asociada TO 'api_publicador'@'%';
+GRANT INSERT, UPDATE, DELETE ON referencia TO 'api_publicador'@'%';
+GRANT INSERT, UPDATE, DELETE ON publicidad TO 'api_publicador'@'%';
+GRANT INSERT, UPDATE, DELETE ON media TO 'api_publicador'@'%';
+
+GRANT INSERT, UPDATE ON motivo_reporte TO 'api_backoffice'@'%';
+GRANT UPDATE ON compra TO 'api_backoffice'@'%';
+GRANT UPDATE ON reventa TO 'api_backoffice'@'%';
+GRANT INSERT, UPDATE ON movimiento TO 'api_backoffice'@'%';
+GRANT UPDATE ON transaccion TO 'api_backoffice'@'%';
+GRANT UPDATE ON publicador TO 'api_backoffice'@'%';
+GRANT INSERT, UPDATE ON oferta TO 'api_backoffice'@'%';
+GRANT INSERT, UPDATE ON genera TO 'api_backoffice'@'%';
+GRANT INSERT, UPDATE ON referencia TO 'api_backoffice'@'%';
+GRANT INSERT, UPDATE ON asociada TO 'api_backoffice'@'%';
+GRANT INSERT, UPDATE ON juego TO 'api_publicador'@'%';
+GRANT INSERT, UPDATE ON clave TO 'api_publicador'@'%';
+GRANT INSERT, UPDATE, DELETE ON resena TO 'api_publicador'@'%';
+GRANT INSERT, UPDATE, DELETE ON reporta_com TO 'api_publicador'@'%';
+GRANT INSERT, UPDATE, DELETE ON reporta_res TO 'api_publicador'@'%';
+GRANT INSERT, UPDATE, DELETE ON reporta_pub TO 'api_publicador'@'%';
+
+GRANT INSERT, UPDATE ON wallet TO 'api_wallet'@'%';
 
 FLUSH PRIVILEGES;
