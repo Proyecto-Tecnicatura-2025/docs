753fbd726458288e696f6fb869b5d96de69f3f33
desmoris <damianesmoris@gmail.com>
2025-10-01 21:36:26 -0300

    Correciones DDL poset consulta

--- db/scripts-sql/01-tienda.sql
+++ db/scripts-sql/01-tienda.sql
@@ -1,30 +1,30 @@
 USE keymasters;
 
-CREATE TABLE Wallet (
+CREATE TABLE wallet(
     id_wallet INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     monto_total DECIMAL(10,2) NOT NULL DEFAULT 0.00
 );
 
-CREATE TABLE Cliente (
+CREATE TABLE cliente(
     id_cliente INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     id_wallet INT UNSIGNED,
     correo VARCHAR(150) NOT NULL,
     es_miembro BOOLEAN NOT NULL DEFAULT FALSE,
     nick VARCHAR(50) NOT NULL,
-    FOREIGN KEY (id_wallet) REFERENCES Wallet(id_wallet)
+    FOREIGN KEY (id_wallet) REFERENCES wallet(id_wallet)
 );
 
-CREATE TABLE Publicador (
+CREATE TABLE publicador(
     id_publicador INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     id_wallet INT UNSIGNED,
     nombre VARCHAR(100) NOT NULL,
     correo VARCHAR(150) NOT NULL,
     aprobado DATETIME NULL,
     reputacion INT NOT NULL DEFAULT 0,
-    FOREIGN KEY (id_wallet) REFERENCES Wallet(id_wallet)
+    FOREIGN KEY (id_wallet) REFERENCES wallet(id_wallet)
 );
 
-CREATE TABLE Juego (
+CREATE TABLE juego(
     id_juego INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     id_publicador INT UNSIGNED NOT NULL,
     genero VARCHAR(50) NOT NULL,
@@ -37,132 +37,129 @@ CREATE TABLE Juego (
     requisitos_sistema TEXT,
     caractersisticas TEXT,
     fecha_lanzamiento DATE,
-    FOREIGN KEY (id_publicador) REFERENCES Publicador(id_publicador)
+    FOREIGN KEY (id_publicador) REFERENCES publicador(id_publicador)
 );
 
-CREATE TABLE Clave (
+CREATE TABLE clave(
     id_clave INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     valor VARCHAR(100) UNIQUE NOT NULL,
     precio DECIMAL(10,2) NOT NULL
 );
 
-CREATE TABLE Publica (
+CREATE TABLE publica(
     id_juego INT UNSIGNED,
     id_clave INT UNSIGNED,
     fecha DATE NOT NULL,
     PRIMARY KEY (id_juego, id_clave),
-    FOREIGN KEY (id_juego) REFERENCES Juego(id_juego),
-    FOREIGN KEY (id_clave) REFERENCES Clave(id_clave)
+    FOREIGN KEY (id_juego) REFERENCES juego(id_juego),
+    FOREIGN KEY (id_clave) REFERENCES clave(id_clave)
 );
 
-CREATE TABLE Transaccion (
+CREATE TABLE transaccion(
     id_transaccion INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     id_wallet_origen INT UNSIGNED,
     id_wallet_destino INT UNSIGNED,
     fecha DATETIME NOT NULL,
     monto DECIMAL(10,2) NOT NULL,
     referencia VARCHAR(255),
-    FOREIGN KEY (id_wallet_origen) REFERENCES Wallet(id_wallet),
-    FOREIGN KEY (id_wallet_destino) REFERENCES Wallet(id_wallet)
+    FOREIGN KEY (id_wallet_origen) REFERENCES wallet(id_wallet),
+    FOREIGN KEY (id_wallet_destino) REFERENCES wallet(id_wallet)
 );
 
-CREATE TABLE Compra (
+CREATE TABLE compra(
     id_compra INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     id_clave INT UNSIGNED NOT NULL,
     id_cliente INT UNSIGNED NOT NULL,
     id_transaccion INT UNSIGNED NOT NULL,
     fecha DATETIME NOT NULL,
     monto DECIMAL(10,2) NOT NULL,
-    estado ENUM(
-        'oculta',
-        'revelada',
-        'reventa'
-    ) NOT NULL DEFAULT 'oculta',
-    FOREIGN KEY (id_clave) REFERENCES Clave(id_clave),
-    FOREIGN KEY (id_cliente) REFERENCES Cliente(id_cliente),
-    FOREIGN KEY (id_transaccion) REFERENCES Transaccion(id_transaccion)
+    estado VARCHAR(100) NOT NULL DEFAULT 'oculta'
+    CONSTRAINT chk_estado_compra CHECK (estado IN ('oculta', 'revendida', 'revelada')),
+    FOREIGN KEY (id_clave) REFERENCES clave(id_clave),
+    FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente),
+    FOREIGN KEY (id_transaccion) REFERENCES transaccion(id_transaccion)
 );
 
-CREATE TABLE Reventa (
+CREATE TABLE reventa(
     id_compra INT UNSIGNED,
     id_cliente INT UNSIGNED,
     fecha_reventa DATETIME NOT NULL,
     precio DECIMAL(10,2) NOT NULL,
     PRIMARY KEY (id_compra, id_cliente),
-    FOREIGN KEY (id_compra) REFERENCES Compra(id_compra),
-    FOREIGN KEY (id_cliente) REFERENCES Cliente(id_cliente)
+    FOREIGN KEY (id_compra) REFERENCES compra(id_compra),
+    FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente)
 );
 
-CREATE TABLE Resena (
+CREATE TABLE resena(
     id_resena INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     id_cliente INT UNSIGNED NOT NULL,
     id_juego INT UNSIGNED NOT NULL,
     fecha DATETIME NOT NULL,
     cuerpo TEXT NOT NULL,
-    FOREIGN KEY (id_cliente) REFERENCES Cliente(id_cliente),
-    FOREIGN KEY (id_juego) REFERENCES Juego(id_juego)
+    FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente),
+    FOREIGN KEY (id_juego) REFERENCES juego(id_juego)
 );
 
-CREATE TABLE Reporta_res (
+CREATE TABLE motivo_reporte(
+    id_motivo INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    descripcion VARCHAR(150) NOT NULL,
+    valido_en VARCHAR(100),
+    CONSTRAINT chk_motivo_reporte_valido_en CHECK (valido_en IN ('resena', 'compra', 'publicacion'))
+);
+
+CREATE TABLE reporta_res(
     id_cliente INT UNSIGNED,
     id_resena INT UNSIGNED,
+    id_motivo INT UNSIGNED,
     fecha DATETIME NOT NULL,
     PRIMARY KEY (id_cliente, id_resena),
-    FOREIGN KEY (id_cliente) REFERENCES Cliente(id_cliente),
-    FOREIGN KEY (id_resena) REFERENCES Resena(id_resena)
+    FOREIGN KEY (id_motivo) REFERENCES motivo_reporte(id_motivo),
+    FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente),
+    FOREIGN KEY (id_resena) REFERENCES resena(id_resena)
 );
 
-CREATE TABLE Reporta_pub (
+CREATE TABLE reporta_pub(
     id_juego INT UNSIGNED,
     id_cliente INT UNSIGNED,
-    motivo ENUM(
-        'explotacion infantil',
-        'contenido para adultos',
-        'difamatorio',
-        'danino',
-        'violacion de la ley',
-        'fraude'
-    ) NOT NULL,
+    id_motivo INT UNSIGNED,
     detalle TEXT,
     fecha DATETIME NOT NULL,
     PRIMARY KEY (id_juego, id_cliente),
-    FOREIGN KEY (id_juego) REFERENCES Juego(id_juego),
-    FOREIGN KEY (id_cliente) REFERENCES Cliente(id_cliente)
+    FOREIGN KEY (id_motivo) REFERENCES motivo_reporte(id_motivo),
+    FOREIGN KEY (id_juego) REFERENCES juego(id_juego),
+    FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente)
 );
 
-CREATE TABLE Reporta_com (
+CREATE TABLE reporta_com(
     id_compra INT UNSIGNED,
     id_cliente INT UNSIGNED,
-    motivo ENUM(
-        'clave no valida',
-        'clave de juego diferente',
-        'clave no soportada en plataforma que decia que si'
-    ) NOT NULL,
+    id_motivo INT UNSIGNED,
     detalle TEXT,
     fecha DATETIME NOT NULL,
     PRIMARY KEY (id_compra, id_cliente),
-    FOREIGN KEY (id_compra) REFERENCES Compra(id_compra),
-    FOREIGN KEY (id_cliente) REFERENCES Cliente(id_cliente)
+    FOREIGN KEY (id_motivo) REFERENCES motivo_reporte(id_motivo),
+    FOREIGN KEY (id_compra) REFERENCES compra(id_compra),
+    FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente)
 );
 
-CREATE TABLE Favorito (
+CREATE TABLE favorito(
     id_cliente INT UNSIGNED,
     id_juego INT UNSIGNED,
     PRIMARY KEY (id_cliente, id_juego),
-    FOREIGN KEY (id_cliente) REFERENCES Cliente(id_cliente),
-    FOREIGN KEY (id_juego) REFERENCES Juego(id_juego)
+    FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente),
+    FOREIGN KEY (id_juego) REFERENCES juego(id_juego)
 );
 
-CREATE TABLE Upovote (
+CREATE TABLE upovote(
     id_cliente INT UNSIGNED,
     id_juego INT UNSIGNED,
     es_positivo BOOLEAN NOT NULL,
     PRIMARY KEY (id_cliente, id_juego),
-    FOREIGN KEY (id_cliente) REFERENCES Cliente(id_cliente),
-    FOREIGN KEY (id_juego) REFERENCES Juego(id_juego)
+    FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente),
+    FOREIGN KEY (id_juego) REFERENCES juego(id_juego)
 );
 
-CREATE TABLE Movimiento (
+CREATE TABLE movimiento(
     id_movimiento INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     id_cliente INT UNSIGNED NOT NULL,
     id_wallet INT UNSIGNED NOT NULL,
@@ -170,33 +167,31 @@ CREATE TABLE Movimiento (
     monto DECIMAL(10,2) NOT NULL,
     concepto VARCHAR(255),
     fecha DATETIME NOT NULL,
-    FOREIGN KEY (id_cliente) REFERENCES Cliente(id_cliente),
-    FOREIGN KEY (id_wallet) REFERENCES Wallet(id_wallet)
+    FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente),
+    FOREIGN KEY (id_wallet) REFERENCES wallet(id_wallet)
 );
 
-CREATE TABLE Oferta (
+CREATE TABLE oferta(
     id_oferta INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     titulo VARCHAR(100) NOT NULL,
-    tipo ENUM(
-        'porcentual',
-        'monto'
-    ) NOT NULL,
+    tipo VARCHAR(1),
     valor DECIMAL(10,2) NOT NULL,
     fecha_inicio DATE NOT NULL,
     fecha_fin DATE NOT NULL,
     activa BOOLEAN NOT NULL DEFAULT TRUE,
-    cupon VARCHAR(50) DEFAULT NULL
+    cupon VARCHAR(50) DEFAULT NULL,
+    CONSTRAINT chk_oferta_tipo CHECK (tipo IN ('p', 'm'))
 );
 
-CREATE TABLE Genera (
+CREATE TABLE genera(
     id_juego INT UNSIGNED,
     id_oferta INT UNSIGNED,
     PRIMARY KEY (id_juego, id_oferta),
-    FOREIGN KEY (id_juego) REFERENCES Juego(id_juego),
-    FOREIGN KEY (id_oferta) REFERENCES Oferta(id_oferta)
+    FOREIGN KEY (id_juego) REFERENCES juego(id_juego),
+    FOREIGN KEY (id_oferta) REFERENCES oferta(id_oferta)
 );
 
-CREATE TABLE Publicidad (
+CREATE TABLE publicidad(
     id_publicidad INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL,
     redireccionamiento VARCHAR(255) NOT NULL,
@@ -206,27 +201,27 @@ CREATE TABLE Publicidad (
     activa BOOLEAN NOT NULL DEFAULT TRUE
 );
 
-CREATE TABLE Referencia (
+CREATE TABLE referencia(
     id_juego INT UNSIGNED,
     id_publicidad INT UNSIGNED,
     PRIMARY KEY (id_juego, id_publicidad),
-    FOREIGN KEY (id_juego) REFERENCES Juego(id_juego),
-    FOREIGN KEY (id_publicidad) REFERENCES Publicidad(id_publicidad)
+    FOREIGN KEY (id_juego) REFERENCES juego(id_juego),
+    FOREIGN KEY (id_publicidad) REFERENCES publicidad(id_publicidad)
 );
 
-CREATE TABLE Asociada (
+CREATE TABLE asociada(
     id_oferta INT UNSIGNED,
     id_publicidad INT UNSIGNED,
     PRIMARY KEY (id_oferta, id_publicidad),
-    FOREIGN KEY (id_oferta) REFERENCES Oferta(id_oferta),
-    FOREIGN KEY (id_publicidad) REFERENCES Publicidad(id_publicidad)
+    FOREIGN KEY (id_oferta) REFERENCES oferta(id_oferta),
+    FOREIGN KEY (id_publicidad) REFERENCES publicidad(id_publicidad)
 );
 
-CREATE TABLE Media (
+CREATE TABLE media(
     id_media INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     id_juego INT UNSIGNED NOT NULL,
     url VARCHAR(255) NOT NULL,
     ubicacion VARCHAR(50) NOT NULL,
     orden INT UNSIGNED DEFAULT NULL,
-    FOREIGN KEY (id_juego) REFERENCES Juego(id_juego)
+    FOREIGN KEY (id_juego) REFERENCES juego(id_juego)
 );
